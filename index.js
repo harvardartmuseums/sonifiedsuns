2b0616801779b16012054c56b7457e3be5c67d68
glevyhas <glevyhas@gmail.com>
2016-11-30 14:23:56 -0500

    bug fixes, changed node.js and express versions

--- index.js
+++ index.js
@@ -1,65 +1,65 @@
 'use strict';
 
 const app = require('express')();
-const io = require('socket.io');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
-const server = app()
-	.use('projectors.html', function(req, res){
-		res.sendFile(path.join(__dirname, 'projectors.html'));
-	})
+app.get('/projectors.html', function(req, res){
+	res.sendFile(path.join(__dirname, '/projectors.html'));
+});
 
-	.use('screens.html', function(req, res){
-		res.sendFile(path.join(__dirname, 'screens.html'));
-	})
+app.get('/screens.html', function(req, res){
+	res.sendFile(path.join(__dirname, '/screens.html'));
+});
 
-	.use('screens.js', function(req, res){
-		res.sendFile(path.join(__dirname, 'screens.js'));
-	})
+app.get('/screens.js', function(req, res){
+	res.sendFile(path.join(__dirname, '/screens.js'));
+});
 
-	.use('convolution/1_criptadisansebastiano.wav', function(req, res){
-		res.sendFile(path.join(__dirname, 'convolution/1_criptadisansebastiano.wav'));
-	})
+app.get('/convolution/1_criptadisansebastiano.wav', function(req, res){
+	res.sendFile(path.join(__dirname, '/convolution/1_criptadisansebastiano.wav'));
+});
 
-	.use('convolution/2_tyndallbrucemonument.wav', function(req, res){
-		res.sendFile(path.join(__dirname, 'convolution/2_tyndallbrucemonument.wav'));
-	})
+app.get('/convolution/2_tyndallbrucemonument.wav', function(req, res){
+	res.sendFile(path.join(__dirname, '/convolution/2_tyndallbrucemonument.wav'));
+});
 
-	.use('convolution/3_falklandpalacebottledungeon.wav', function(req, res){
-		res.sendFile(path.join(__dirname, 'convolution/3_falklandpalacebottledungeon.wav'));
-	})
+app.get('/convolution/3_falklandpalacebottledungeon.wav', function(req, res){
+	res.sendFile(path.join(__dirname, '/convolution/3_falklandpalacebottledungeon.wav'));
+});
 
-	.use('convolution/4_centralhalluniversityofyork.wav', function(req, res){
-		res.sendFile(path.join(__dirname, 'convolution/4_centralhalluniversityofyork.wav'));
-	})
+app.get('/convolution/4_centralhalluniversityofyork.wav', function(req, res){
+	res.sendFile(path.join(__dirname, '/convolution/4_centralhalluniversityofyork.wav'));
+});
 
-	.use('convolution/5_castellodegualtieriis.wav', function(req, res){
-		res.sendFile(path.join(__dirname, 'convolution/5_castellodegualtieriis.wav'));
-	})
+app.get('/convolution/5_castellodegualtieriis.wav', function(req, res){
+	res.sendFile(path.join(__dirname, '/convolution/5_castellodegualtieriis.wav'));
+});
 
-	.use('convolution/6_stmargaretschurch.wav', function(req, res){
-		res.sendFile(path.join(__dirname, 'convolution/6_stmargaretschurch.wav'));
-	})
+app.get('/convolution/6_stmargaretschurch.wav', function(req, res){
+	res.sendFile(path.join(__dirname, '/convolution/6_stmargaretschurch.wav'));
+});
 
-	.use('convolution/7_kinoullaisle.wav', function(req, res){
-		res.sendFile(path.join(__dirname, 'convolution/7_kinoullaisle.wav'));
-	})
+app.get('/convolution/7_kinoullaisle.wav', function(req, res){
+	res.sendFile(path.join(__dirname, '/convolution/7_kinoullaisle.wav'));
+});
 
-	.use('convolution/8_yorkminster.wav', function(req, res){
-		res.sendFile(path.join(__dirname, 'convolution/8_yorkminster.wav'));
-	})
+app.get('/convolution/8_yorkminster.wav', function(req, res){
+	res.sendFile(path.join(__dirname, '/convolution/8_yorkminster.wav'));
+});
 
-	.use('convolution/9_arbroathabbeysacristy.wav', function(req, res){
-		res.sendFile(path.join(__dirname, 'convolution/9_arbroathabbeysacristy.wav'));
-	})
+app.get('/convolution/9_arbroathabbeysacristy.wav', function(req, res){
+	res.sendFile(path.join(__dirname, '/convolution/9_arbroathabbeysacristy.wav'));
+});
 
-	.use('js/config.js', function(req, res){
-		res.sendFile(path.join(__dirname, 'js/config.js'));
-	})
+app.get('/js/config.js', function(req, res){
+	res.sendFile(path.join(__dirname, '/js/config.js'));
+});
 
-	.listen(PORT, () => console.log('Listening on ${ PORT }'));
+server.listen(PORT);
 
 
 var screensIO = io.of('/screens-namespace');
