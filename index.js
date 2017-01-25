b2a9f48189d49012283103b0cb6d1ae567f12aec
glevyhas <glevyhas@gmail.com>
2017-01-25 13:32:13 -0500

    Updated timeout on socket connection

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ function testConnection() {
 
 screensIO.on('connection', function(socket) {
 	if (unconnectedScreen) {
-		socket.broadcast.to(socket.id).emit('too many sockets');
+		io.to(socket.id).emit('too many sockets');
 	} else { 
 		if (unconnectedProjector) {
 			unconnectedProjector = 0;
