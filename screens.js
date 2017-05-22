e987c75719f4ef1c0b2cdbe45edf599465b27445
glevyhas <glevyhas@gmail.com>
2017-05-22 12:50:12 -0400

    adjusted iPad messages

--- screens.js
+++ screens.js
@@ -501,7 +501,7 @@ function doWork() {
 		// upon request, give description of project
 		if (explanation == true) {
 
-			socket.emit('message', 'The tones you hear relate to the colors of the object being described.<br /><br />Low notes are colors at the beginning of the rainbow, like <font color="red">red</font> and <font color="orange">orange</font>.<br />High notes are colors at the end of the rainbow, like <font color="blue">blue</font> and <font color="purple">purple</font>.<br /><br />Notes that play sooner represent more of the object\'s color.<br /><br /><font size="90%">Small sounds are small objects.</font><br /><font size="110%">Big sounds are big objects.</font><br /><br /><font color="blue">Louder sounds are brighter colors.</font><br /><font color="greyblue">Softer sounds are duller colors.</font>');
+			socket.emit('message', 'The tones you hear relate to the colors of the object being described.<br /><br />Low notes are colors at the beginning of the rainbow, like <font color="red">red</font> and <font color="orange">orange</font>.<br />High notes are colors at the end of the rainbow, like <font color="blue">blue</font> and <font color="purple">purple</font>.<br /><br />Notes that play sooner represent more of the object\'s color.<br /><br /><font size="90%">Small sounds are small objects.</font><br /><font size="110%">Big sounds are big objects.</font><br /><br /><font color="#00b300">Louder sounds are brighter colors.</font><br /><font color="#3e743e">Softer sounds are duller colors.</font>');
 
 			for (var i = 0; i < explanationSpeech.length; i++) {
 				synthesis.speak(explanationSpeech[i]);
